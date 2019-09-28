1ec12acaba59effe478d5e22f172d02bf73cda58
margie <ly.margarett@gmail.com>
2019-09-28 13:47:37 -0700

    create structure for controller

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,15 @@
+const task = require('../models/TaskModel.js');
+
 module.exports = {
+  postTask = (req, res, next) => {
+    task(req.body.data);
+  };
 
+  getTask = (req, res, next) => {
+    // should retrieve all items from the database and send it back to the client as JSON
+  };
 
+  deleteTask = (req, res, next) => {
+    // should find items in the database based on an ID number and delete that item if it exists
+  };
 };
