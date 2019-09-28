0e26ac90f220636cc5dedb003af8c7bb5ead266e
Patrick Young <patrick.shaffer.young@gmail.com>
2019-09-28 16:26:34 -0700

    My App

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,32 @@
-module.exports = {
 
+const taskController = {};
 
-};
+taskController.postTask = (req, res, next) => {
+  console.log(`postTask with id: ${req.body.description}`);
+
+  next();
+}
+// dummy data
+let dummyTasksId = 14;
+const dummyTasks = {tasks: 
+    [
+      {item: 'Clean up your room.', created_at: Date.now(), id: 10},
+      {item: 'Go to the gym.', created_at: Date.now(), id: 11},
+      {item: 'Watch a movie.', created_at: Date.now(), id: 12}
+    ]}
+
+taskController.getTasks = (req, res, next) => {
+    res.locals.getTasks = dummyTasks;
+  
+    next();
+}
+
+  
+taskController.deleteTask = (req, res, next) => {
+    console.log(`deleteTask with id: ${req.body.id}`);
+  
+    next();
+}
+  
+
+module.exports = taskController;
