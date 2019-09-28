f6b3f61721bd32ecb41f9fbdac4fe62243e832e8
kbsjsn <shin.kirk@gmail.com>
2019-09-28 16:12:32 -0700

    attempt grad-assess-part2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,18 @@
 module.exports = {
-
+  checkUserPass: (req, res, next) => {
+    const { user, pass } = req.body;
+    if(user === 'codesmith' && pass === 'ilovetesting') {
+      res.cookie('token', 'admin');
+      return next();
+    }
+    res.send('unsuccessful login attempt');
+  },
+  checkCookie: (req, res, next) => {
+    const { token } = req.cookies;
+    if(token === 'admin') {
+      return next();
+    }
+    res.send('You must be signed in to view this page');
+  } 
 
 };
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,25 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://hlxlcxjs:k-_SNnIVm_kz6wWnxcn5TbCmBVEdwYYp@salt.db.elephantsql.com:5432/hlxlcxjs';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: URI
+});
+
+const queryString = `CREATE TABLE IF NOT EXISTS Tasks (
+  id SERIAL PRIMARY KEY, 
+  item VARCHAR, 
+  created_at TIME WITH TIME ZONE DEFAULT NOW())`
+
+pool.query(queryString)
+  // .then(res => res.json())
+  .then(data => console.log('successfully created table'))
+  .catch(e => console.error('unsuccessful table created', e))
 
 
-
-
-module.exports = null; // <-- export your model
+module.exports = pool; // <-- export your model
