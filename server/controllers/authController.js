aa6ccd1da266e34f1268a15e354d0fabe96fb39e
Reid Klarsfeld <reidklarsfeld@reids-mbp.home>
2019-09-28 16:39:20 -0700

    assessment part 2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,13 @@
-module.exports = {
+const authController = {}
 
+//thought here was to check cookie, if value was admin, rediret to /secret. Else, send over a new cookie if 
+//username && password matched codesmith && ilovetesting and then redirect to /secret
+authController.verify = (req, res, next) => {
+    if(req.cookies.token === 'admin') res.redirect('/secret')
+    if(req.body.username === 'codesmith' && req.body.pasword === 'ilovetesting'){
+        res.cookie('token', 'admin')
+        next();
+    }
+}
 
-};
+module.exports = authController;
