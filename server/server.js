8a4272471a54b3d383060908ed7c5a19e79d620e
Ken S <gxcadr@gmail.com>
2019-09-28 16:21:50 -0700

    get tasks working, trying to get add task

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express(); // preparing express for use
 const bodyParser = require('body-parser'); // may need
+const cookieParser = require('cookie-parser');
 const path = require('path'); // may need
 const PORT = 3333 // PORT 3333 as requested
+const taskController = require('./controllers/taskController');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use('/views', express.static(path.resolve(__dirname, '../views/')));
 app.use('/css', express.static(path.resolve(__dirname, '../assets/css')));
@@ -12,11 +17,30 @@ app.use('/js', express.static(path.resolve(__dirname, '../assets/js')));
 app.get('/', function (req, res) {
   res.status(200);
   res.sendFile(path.resolve(__dirname, '../views/index.html'));
-})
+});
+
+app.get('/tasks', taskController.getTasks, function (req, res) {
+  console.log('final callback on /tasks hit', res.locals.tasks);
+  res.status(200);
+  res.json(res.locals.tasks);
+});
+
+app.post('/tasks', function (req, res) {
+  console.log('final callback on post /tasks hit');
+  res.send('task added');
+});
 
 app.get('/secret', function (req, res) {
   res.status(200);
   res.sendFile(path.resolve(__dirname, '../views/secret.html'));
-})
+});
 
-app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
+app.use('*', function (req, res) {
+  res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send('Global error handler hit');
+});
+
+app.listen(PORT, () => console.log(`this FANTASTIC server is listening on port ${PORT}`));
