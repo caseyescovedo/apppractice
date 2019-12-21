08938ba3ecdc2b5535a0e2b6622c31a38d3f1ebf
Andrew Shin <andyrpg@gmail.com>
2019-12-21 12:29:16 -0800

    had problems with mongoDB, but overall logic makes sense. Problems sending the req.body

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,17 @@
-module.exports = {
+const authController = {};
 
+authController.login = (req, res, next) => {
+    const { user, pass } = req.body;
+    if (user === "codesmith" && pass === "ilovetesting") { 
+        return next();
+    } else { 
+        return next("Please enter valid login and password");
+    }
+}
 
-};
+authController.addCookie = (req, res, next) => {
+    res.cookie('access', 'true')
+    return next();
+}
+
+module.exports = authController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,21 @@
+const mongoose = require('mongoose');
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'mongodb+srv://andrewsnap:lol@cluster0-vpsa9.mongodb.net/test?retryWrites=true&w=majority';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
+mongoose.connect(myURI, {useNewUrlParser: true,  useUnifiedTopology: true});
 
+const Schema = mongoose.Schema;
 
+const tasksSchema = new Schema({
+    item: { type: String, required: true },
+    create_at: { type: Date, default: Date.now() } 
+})
 
+const Task = mongoose.model('Tasks', tasksSchema)
 
-module.exports = null; // <-- export your model
+module.exports = Task; // <-- export your model
