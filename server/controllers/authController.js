e4fdc89e6668c2f5c9fbf3bb928b5c624392eb4c
Jourdan Ball <jourdan.ball@gmail.com>
2019-12-21 12:36:40 -0800

    Assessment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,25 @@
-module.exports = {
+const authController = {}
 
 
-};
+authController.loginCheck = (req, res, next) => {
+    if(req.body.user === 'codesmith' && req.body.pass === 'ilovetesting'){
+        res.cookie('token', 'admin')
+        res.redirect('/secret');  
+        next();
+    } else {
+        res.send('unsuccessful login attempt')
+    }
+}
+// authController.cookieCheck = (req, res, next) => {
+//         if(req.cookies.token === 'admin'){
+//             res.redirect()
+//         next();
+//     } else {
+//         res.send('You must be signed in to see this page.')
+//     }
+// }
+
+module.exports = authController
+
+
+
