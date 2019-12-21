bf117836681ad331476cbb8303bb74a53da2b9ee
evanking11 <evanking11@gmail.com>
2019-12-21 12:54:52 -0800

    duplicate request issue

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,45 @@
-module.exports = {
+const models = require("../models/TaskModel");
 
+const taskController = {};
 
+taskController.getTasks = (req, res, next) => {
+  models.Task.find({}, function(err, result) {
+    if (err) next(err);
+    res.locals.tasks = result;
+    return next();
+  });
 };
+
+taskController.postTask = (req, res, next) => {
+  console.log(req.body);
+  const { item, created_at } = req.body;
+  console.log("created_at is a", typeof created_at);
+  models.Task.create({ item, created_at })
+    .then(result => {
+      res.locals.tasks = result;
+      console.log("task was saved as: ", result);
+      next();
+    })
+    .catch(err => {
+      next({
+        log: `taskController.postTask: ERROR: ${err}`,
+        message: {
+          err:
+            "Error occurred in taskController.postTask. Check server logs for more details."
+        }
+      });
+    });
+};
+
+taskController.deleteTask = (req, res, next) => {
+  console.log("inside delete task");
+  let id = req.params.id;
+  models.Task.remove({ id }, (err, result) => {
+    if (err)
+      return next("Error in taskController.deleteTask: " + JSON.stringify(err));
+    res.locals.task = task;
+    return next();
+  });
+};
+
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,33 @@
+const mongoose = require("mongoose");
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI =
+  "mongodb+srv://max:maxpass@cluster0-dpmfw.mongodb.net/test?retryWrites=true&w=majority";
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
 
+mongoose
+  .connect(myURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "assessment2"
+  })
+  .then(() => console.log("Connected to Mongo DB."))
+  .catch(err => console.log(err));
 
+const Schema = mongoose.Schema;
 
+const taskSchema = new Schema({
+  item: { type: String, required: true },
+  created_at: { type: Date, default: Date.now }
+});
 
-module.exports = null; // <-- export your model
+const Task = mongoose.model("task", taskSchema);
+
+module.exports = {
+  Task
+}; // <-- export your model
