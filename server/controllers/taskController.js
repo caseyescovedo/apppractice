045ecd9323e7832333f1338b891f2e6b61ddc234
Luke Van Bergen <lukepvb@gmail.com>
2020-01-11 16:32:59 -0800

    current progress

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,74 @@
-module.exports = {
+// CREATE MIDDLEWARE IN THIS FILE, EXPORT AND PULL IN ON SERVER.JS
 
+// require in Task model
+const Task = require('../models/TaskModel');
 
+// const taskController = {};
+const taskController = {};
+
+// *** POST a new task to the database (create new task, and POST it) ****
+taskController.postTask = (req, res, next) => {
+  // pull the item key that is stored on the request body using deconstruction
+  const { item } = req.body;
+  // create a new item using the name pulled from the request body
+  Task.create({ item })
+    // store the item in res.locals.task
+    .then(item => {
+      res.locals.task = item;
+      console.log('New task added to the database!');
+      // Its not clear if you want all of this done on this page, but I am going to send the response back to the front end from server.js because
+      // it looks cleaner and I am more familiar with that format
+      // res.status(200).json(res.locals.task); /
+      return next();
+    })
+    .catch(err => {
+      return next({
+        log: `ERROR: taskController.createTask: ERROR ${err}`,
+        message: `ERROR: taskController.createTask: ERROR see server log for details`
+      });
+    });
 };
+
+// *** GET all tasks from the database ***
+taskController.getTasks = (req, res, next) => {
+  Task.find({})
+    .exec()
+    .then(tasks => {
+      console.log(tasks);
+      res.locals.tasks = tasks;
+      return next();
+    })
+    .catch(err => {
+      return next({
+        log: `ERROR: taskController.getTasks: ERROR ${err}`,
+        message: `ERROR: taskController.getTasks: ERROR see server log for details`
+      });
+    });
+};
+
+// *** DELETE an item from the database by :id ***
+
+// deleting functionality seems to work, but will need to come back to this. Spending too much time here
+
+taskController.deleteTask = (req, res, next) => {
+  // console.log(req.params);
+  const { id } = req.params;
+
+  Task.findOneAndDelete(id)
+    .exec()
+    .then(item => {
+      // console.log(item);
+      item.remove();
+      res.locals.removed = item;
+      // console.log('Task Removed!');
+      return next();
+    })
+    .catch(err => {
+      return next({
+        log: `ERROR: taskController.deleteTask: ERROR: ${err}`,
+        message: `ERROR: taskController.deleteTask: ERROR: see server log for details`
+      });
+    });
+};
+
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Create a Task model
+const TaskSchema = new Schema({
+  item: { type: String, required: true },
+  created_at: { type: Date, default: Date.now }
+});
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI =
+  'mongodb+srv://lukepvb:12345@todo-kp5qx.mongodb.net/test?retryWrites=true&w=majority';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
+// console.log(URI);
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
 
+// So we are not supposed to connect to our database? This is confusing. I would usually create and export URI from a seperate folder
+const Task = mongoose.model('task', TaskSchema);
+// console.log(Task);
 
-
-
-module.exports = null; // <-- export your model
+// module.exports = Task = mongoose.model('task', TaskSchema); // <-- export your model
+module.exports = Task;
