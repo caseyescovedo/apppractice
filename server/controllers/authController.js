e68652170c62994f1f8e7b8c9cf1b152e9fbcfe6
Cornelius Phanthanh <corneliusphanthanh@gmail.com>
2020-01-11 16:31:34 -0800

    part 2 of assessment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,13 @@
-module.exports = {
+const authController = {};
 
+authController.verifyUser = (req, res, err) => {
+  console.log('in the authcontroller.verifyuser top')
+  console.log(req.body.user, req.body.pass);
+  if (req.body.user === 'codesmith' && req.body.pass === 'ilovetesting') {
+    res.redirect('/secret');
+  }
+  res.status(418).send('unsuccessful login attempt')
+  res.redirect('/index')
+}
 
-};
+module.exports = authController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,38 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const mongoose = require('mongoose');
+const myURI = 'mongodb+srv://corneeltron:mongolia87@cluster0-1rk9e.mongodb.net/test?retryWrites=true&w=majority'
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
-// UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+// Connect to MongoDB
+mongoose.connect(URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // sets the name of the DB that our collections are part of
+  dbName: 'exam-database'
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
 
 
+const Schema = mongoose.Schema;
+
+const TasksSchema = new Schema({
+  item: {
+    type: String,
+    require: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Task = mongoose.model("task", TasksSchema);
 
 
-module.exports = null; // <-- export your model
+//remember export model
+
+module.exports = Task;
