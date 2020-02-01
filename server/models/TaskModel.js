d9e6f75f188712800cbee90332b9b8147e034075
Matt Peters <matthew.george.peters@gmail.com>
2020-02-01 12:42:17 -0500

    added pg, setup SQL model for tasks

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,20 @@
+const { Pool } = require('pg');
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://qtrjnoyf:StzWFjt63gCOCPgHD8S4hgKJBGL3gE4Z@rajje.db.elephantsql.com:5432/qtrjnoyf';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+const pool = new Pool({
+  connectionString: URI,
+})
 
-
-
-module.exports = null; // <-- export your model
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  }
+}; // <-- export your model
