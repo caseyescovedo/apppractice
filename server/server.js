585216719893940097a7bd9977d24043bd616939
Matt Peters <matthew.george.peters@gmail.com>
2020-02-01 12:58:59 -0500

    serving static html

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -8,8 +9,10 @@ const PORT = 3333;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 app.post('/', (req, res) => {
