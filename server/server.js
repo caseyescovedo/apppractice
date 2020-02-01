8b36d6376fb7bdfda7666b6dd87657fe226f8773
Matt Peters <matthew.george.peters@gmail.com>
2020-02-01 12:52:04 -0500

    installed and added path and body-parser

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
 const PORT = 3333;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request');
+});
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user');
+});
 
 app.listen(PORT, () => console.log(`To-Do list app listening to port ${PORT}`));
