cb940dc971d961d2ada9c57cca301e3a655e4017
Sergiy Alariki <sergiy.alariki.21@gmail.com>
2020-02-01 15:58:46 -0500

    all functionality

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const taskController = require('./controllers/taskController');
+const authController = require('./controllers/authController');
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 
 app.get('/tasks', taskController.getTasks, (req, res) => {
     res.type('application/json');
@@ -23,6 +25,10 @@ app.delete('/task', taskController.deleteTask, (req, res) => {
     res.sendStatus(200);
 });
 
+app.post('/signin', authController.authenticate, (req, res) => {
+    res.status(200).redirect('/secret');
+});
+
 
 app.use(express.static('assets'));
 app.get('/', (req, res) => {
@@ -30,8 +36,12 @@ app.get('/', (req, res) => {
     res.status(200).sendFile(path.resolve(__dirname, '../views/index.html'));
 });
 app.get('/secret', (req, res) => {
-    res.set('Content-Type', 'text/html');
-    res.status(200).sendFile(path.resolve(__dirname, '../views/secret.html'));
+    console.log(req.cookies.token);
+    if (req.cookies.token === 'admin') {
+        res.set('Content-Type', 'text/html');
+        res.status(200).sendFile(path.resolve(__dirname, '../views/secret.html'));
+    }
+    else res.status(200).json('You must be signed in');
 });
 
 
