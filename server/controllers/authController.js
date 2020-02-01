f13e3aeeab7caa3af06631e9c5888d2ba25dba16
Matt Peters <matthew.george.peters@gmail.com>
2020-02-01 15:59:32 -0500

    auth logic works

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,8 +7,9 @@ const authController = {};
 
 authController.authenticate = (req, res, next) => {
   console.log('authenticate req body', req.body);
-  if (authObject.user !== req.body.user && authObject.pass !== req.body.user) {
+  if (authObject.user !== req.body.user && authObject.pass !== req.body.pass) {
     res.status(401).send('unsuccessful login attempt');
+    next();
   }
   next();
 };
