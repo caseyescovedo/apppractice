e44834d338ceac3ca4d8a902e3acf35a9c944008
David Lee <dave@Davids-MacBook-Pro.local>
2020-02-01 16:29:47 -0500

    commit

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,28 @@
+const { Pool } = require('pg')
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const PG_URI = 'postgres://dqsvyaly:YaBPNOzHJ7ivreh6gOnZMQxNIVdT01Td@rajje.db.elephantsql.com:5432/dqsvyaly';
+
+const pool = new Pool({
+  connectionString: PG_URI
+})
+
+const createTableString = 'CREATE TABLE IF NOT EXISTS Tasks (_id SERIAL PRIMARY KEY, item VARCHAR, created_at TIMESTAMP)'
+
+pool.query(createTableString, (err) => {
+  if (err) console.log('Error when creating table')
+})
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || PG_URI;
 
 
 
 
-module.exports = null; // <-- export your model
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  }
+} // <-- export your model
