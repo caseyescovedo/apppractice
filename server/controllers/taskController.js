2e48eb2d1736f49b396694ac2ab571de1eea4c8a
Josh Naso <joshnaso@Joshs-MacBook-Pro.local>
2020-02-01 16:21:33 -0500

    Assesment part 2

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,46 @@
-module.exports = {
+const db = require('../models/TaskModel.js');
 
+const taskController = {};
 
-};
+taskController.postTask = function(req, res, next){
+    console.log("The body: ", req.body);
+    const { task } = req.body;
+    console.log("The task: ", task);
+    const query = `INSERT INTO Task(item) VALUES($1);`;
+    const params = [task];
+
+    db.query(query, params)
+    .then((data) => {
+        next();
+    })
+    .catch((err) => {
+        next(err);
+    })
+}
+
+taskController.getTask = function(req, res, next){
+    const query =  `SELECT * FROM Task;`;
+    db.query(query, [])
+    .then((data) => {
+        res.locals.task = data.rows;
+        next();
+    })
+    .catch((err) => {
+        next(err);
+    })
+}
+
+taskController.deleteTask = function(req, res, next){
+    const { task } = req.body;
+    const query =  `DELETE FROM Task WHERE item = $1;`;
+    const params = [task];
+    db.query(query, params)
+    .then((data) => {
+        next();
+    })
+    .catch((err) => {
+        next(err);
+    })
+}
+
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,25 @@
+const { Pool } = require('pg');
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://epdhxvyy:ayJRWk6xIukvJb8-5tzANg6fEQDAQ0W8@rajje.db.elephantsql.com:5432/epdhxvyy';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+const pool = new Pool({
+    connectionString: myURI,
+  });
+  
+pool.query(`CREATE TABLE IF NOT EXISTS Task(
+    item VARCHAR(255) NOT NULL, 
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
+);`);
 
-
-
-module.exports = null; // <-- export your model
+module.exports = { 
+    query: function(query, params, func){
+    return pool.query(query, params, func);
+  },
+}; // <-- export your model
