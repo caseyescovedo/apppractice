e18e8c822c1a3ca9d559e6074873dece36c22055
Kelvin Cuesta <kelvinscuesta@gmail.com>
2020-02-01 13:53:43 -0500

    Getting delete task functionality

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,56 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+
+// installed pg 
+// will be using postgres and using elephantSQL to host my database
+const myURI = 'postgres://arqsswpq:TExFYG6nMRwKLK9gf1_Uy-yUV55ttvNr@rajje.db.elephantsql.com:5432/arqsswpq';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
 
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: myURI,
+});
 
-module.exports = null; // <-- export your model
+
+
+// Made my tables in terminal 
+// want to make a users table and a tasks table
+/**
+ * psql 'postgres://arqsswpq:TExFYG6nMRwKLK9gf1_Uy-yUV55ttvNr@rajje.db.elephantsql.com:5432/arqsswpq'
+ * 
+ * Using vim editor \e to input query strings in psql shell
+ * 
+ * \e 
+ * 
+ * CREATE TABLE Users(
+ *  user__id serial PRIMARY KEY,
+ *  username VARCHAR (50) UNIQUE NOT NULL,
+ *  password VARCHAR (50) NOT NULL,
+ * )
+ * 
+ * CREATE TABLE Tasks(
+ * __id serial PRIMARY KEY,
+ * user_id INTEGER REFERENCES Tasks (user_id) DEFAULT 1
+ * item VARCHAR NOT NULL,
+ * created_at TIMESTAMPZ DEFAULT NOW()
+ * );
+ * 
+ * get my neighborhood friendly codesmith in there
+ * INSERT INTO users (username, password) VALUES ('codesmith', 'ilovetesting');
+ * 
+ */
+
+
+// exporting my model object
+module.exports = {
+  query: (queryString, queryValues, callback) => {
+    console.log('EXECUTED', queryString);
+    return pool.query(queryString, queryValues, callback);
+  },
+};
---
+++ server/server.js
@@ -0,0 +1,66 @@
+// express installed
+// pg installed
+// body parser installed
+// path imported 
+const express = require('express');
+const app = express();
+const PORT = 3333;
+const bodyParser = require('body-parser');
+const path = require('path');
+const taskController = require('./controllers/taskController');
+
+// parse that body baby
+app.use(bodyParser());
+// FLOW TEST
+app.use((req, res, next) => {
+  console.log(`********* KELVIN FLOW TEST***********
+  URL: ${req.url}
+  METHOD: ${req.method}
+  BODY: ${JSON.stringify(req.body)}\n`);
+
+  return next();
+});
+
+
+
+
+// with express static the content-type header is automatically assumed and works after checking chrome dev tools
+app.use(express.static('assets'));
+
+// when visiting localhost:3333/ should render the index.html file from views
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../views/index.html'));
+});
+
+// get the secret page to render when hitting this endpoint
+app.get('/secret', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../views/secret.html'))
+});
+
+
+// basic test route to getTasks
+app.get('/getTasks', taskController.getTasks, (req, res) => {
+  res.json(res.locals.tasks);
+})
+
+// basic test route to postTasks
+app.post('/postTask', taskController.postTask, (req, res) => {
+  res.json(res.locals.taskAdded);
+})
+
+
+
+// handle wildcard requests to nonexistent endpoints
+app.use('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+// global error handler to catch errs in any middleware
+// clean this up
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.json(err);
+});
+
+// server up and listening 
+app.listen(PORT, () => { console.log(`LISTENING ON PORT ${PORT}`) });
