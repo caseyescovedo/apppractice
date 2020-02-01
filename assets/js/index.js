fcaf2f4e6fd526266d87acdb18dcd63e3adb0e56
Matt Peters <matthew.george.peters@gmail.com>
2020-02-01 14:25:59 -0500

    got li to display

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,26 @@ console.log('testtt');
 
 const getTasks = document.getElementById('retrieve');
 
+const taskList = document.getElementById('task-list');
+
+const create = document.getElementById('create');
+
 getTasks.addEventListener('click', () => {
-  console.log('clicked')
+  fetch('/tasks/getTasks')
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+
+      taskList.innerHTML = '';
+
+      response.forEach((resp) => {
+        const taskDiv = document.createElement('li');
+        taskDiv.innerHTML = `${resp.item}<button class='remove'>X</button>`;
+        taskList.appendChild(taskDiv);
+      });
+
+    })
+    .catch((error) => {
+      console.log('Error fetching Tasks from client. Error: ', error);
+    });
 })
