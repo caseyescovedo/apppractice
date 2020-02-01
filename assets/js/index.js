d77d17e5782d324041c394bfbd7bc791e65912f6
Matt Peters <matthew.george.peters@gmail.com>
2020-02-01 14:42:05 -0500

    added event listener to each individuel li element

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,10 +16,16 @@ getTasks.addEventListener('click', () => {
 
       response.forEach((resp) => {
         const taskDiv = document.createElement('li');
-        taskDiv.innerHTML = `${resp.item}<button class='remove'>X</button>`;
+        taskDiv.innerHTML = `${resp.item}<button id="${resp.message_id}" class="remove">X</button>`;
         taskList.appendChild(taskDiv);
       });
 
+      document.querySelectorAll('.remove').forEach((item) => {
+        item.addEventListener('click', (e) => {
+        // e.preventDefault();
+          console.log('e is clicked!', e.target);
+        });
+      });
     })
     .catch((error) => {
       console.log('Error fetching Tasks from client. Error: ', error);
