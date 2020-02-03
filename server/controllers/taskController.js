fee7573d088f994873e3e0dfab503c81239dae3a
chelseyeslehc <chelsey.fewer@gmail.com>
2020-02-03 15:18:48 -0500

    assessment actual files

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,58 @@
-module.exports = {
+//require in our database connection so that we can use it here
+const db = require('../models/TaskModel.js');
 
+const taskController = {};
 
+taskController.postTask = function(req, res, next) {
+  console.log('req.body.item in postTask: ', req.body.item);
+  //task should come in via user input in the req.body so we'll pull it out of there
+  const { item } = req.body;
+  //to avoid the use of template literals, we'll add task to an array and pass it into our SQL query
+  const arr = [item];
+  const queryString = 'INSERT INTO Task (item) VALUES ($1)';
+  db.query(queryString, arr, (err) => {
+    if (err) {
+      // ** add global error handler that we can pass these errors to instead of just logging them in the middleware
+      console.log('error in postTask: ', err);
+      return next(err);
+    }
+    //move onto the next piece of middlware
+    return next();
+  });
 };
+
+taskController.getTasks = function(req, res, next) {
+  const queryString = 'SELECT * FROM Task';
+  db.query(queryString, (request, response, err) => {
+    if (err) {
+      // ** add global error handler that we can pass these errors to instead of just logging them in the middleware
+      console.log('error in getting all tasks from the db: ', err);
+      return next(err);
+    } else {
+      console.log('response.rows in getTasks: ', response.rows);
+      res.locals.tasks = response.rows;
+    }
+    //move onto the next piece of middlware
+    return next();
+  });
+};
+
+taskController.deleteTask = function(req, res, next) {
+  console.log('req.body.id in deleteTask: ', req.body.id);
+  //we'll use the id from the req.body to tell us which task we need to delete and then pass that into an array which will be used in our SQL query
+  const { id } = req.body;
+  const arr = [id];
+  const queryString = 'DELETE from task WHERE id=$1';
+  db.query(queryString, arr, (request, response, err) => {
+    if (err) {
+      // ** add global error handler that we can pass these errors to instead of just logging them in the middleware
+      console.log('error in deleting task: ', err);
+      return next(err);
+    }
+    //move onto the next piece of middlware
+    console.log('deleted entry');
+    return next();
+  });
+};
+
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,35 @@
-// v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const { Pool } = require('pg');
 
-// UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const myURI =
+  'postgres://sypekhae:g6QRzfih0khnkER1LeHdns51iTR3gMnI@rajje.db.elephantsql.com:5432/sypekhae';
 
-// UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+//connect to the database:
+//pool takes a k:v pair to take in the connectionString for the db
+const pool = new Pool({
+  connectionString: URI
+});
 
+// create the table by running a db.query with the queryString initially
+// ** not sure how to have created_at select the specific time each time a new entry is made to the database
+// const date = Date.now();
+// const arr = [date];
+const Task =
+  'CREATE TABLE IF NOT EXISTS task (id SERIAL PRIMARY KEY, item VARCHAR NOT NULL, created_at DATE)';
+//query to create the table once:
+pool.query(
+  Task,
+  /*arr,*/ (err) => {
+    if (err) console.log('there was an error creating the Task table');
+    console.log('connected to the database');
+  }
+);
 
-
-module.exports = null; // <-- export your model
+//export the database so that we can query it elsewhere
+module.exports = {
+  query(queryString, params, callback) {
+    console.log('ran a query: ', queryString);
+    return pool.query(queryString, params, callback);
+  }
+};
