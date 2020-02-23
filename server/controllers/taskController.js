82a6edfd35f0d5d73234d2444fac1d0ca70ace68
Nicole Abramowski <nabramow@gmail.com>
2020-02-23 17:56:20 -0500

    Refactor auth and task controllers

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,33 +1,29 @@
 const Task = require('../models/TaskModel.js');
 
-// I know I'm supposed to be routing my routes from server.js here but I couldn't get this
-// working. I kept getting the error TaskController.getTasks is not a function, etc.
-// I couldn't figure it out as they ARE functions. I tried going TaskController.getTasks
-// here instead of the current way of having one const up top with individual functions within.
-// I made sure I am requiring in the TaskController page in server.js. I tried exporting the 
-// individual functions as well but eventually gave up to save time and doing it all in the
-// server.js page
+const TaskController = {};
 
-const TaskController = {
-
-  // create new item in database
-  // TaskController.postTask((req, res, next) => {
-  //   // functionality here
-  // });
-
-  // get tasks from the database
-  getTasks(req, res, next) {
-  // use req.params to get
-    next();
-  },
-
-  // // delete a specific task from the database
-  // TaskController.deleteTask((req, res, next) => {
-  //   // use req.params to delete
-  // });
-
+// get tasks from the database
+TaskController.getTasks = (req, res, next) => {
+  console.log('made it to getTasks');
+  Task.find((err, results) => {
+    if (err) return console.log(err);
+    res.json(results);
+    return next();
+  });
 };
 
-module.exports = {
-  TaskController,
+// create new item in database
+TaskController.postTask = (req, res) => {
+  Task.create(req.body, (err, results) => {
+    if (err) console.log(err);
+  });
 };
+
+// delete a specific task from the database
+TaskController.deleteTask = (req, res, next) => {
+  Task.findByIdAndDelete(req.params.id, (err, results) => {
+    if (err) console.log(err);
+  });
+};
+
+module.exports = TaskController;
--- server/server.js
+++ server/server.js
@@ -1,86 +1,58 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
-const Task = require('./models/TaskModel.js');
+const AuthController = require('./controllers/authController.js');
 const TaskController = require('./controllers/taskController.js');
+
 const app = express();
-app.use(cookieParser());
 const PORT = 3333;
 
-const db = mongoose.connect('mongodb+srv://nabramow:gradmongo!@cluster0-ufsoo.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err, database) => {
+
+// ! Where should this go? In server or here?
+mongoose.connect('mongodb+srv://nabramow:gradmongo!@cluster0-ufsoo.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, (err, database) => {
   if (err) return console.log(err);
   console.log('connected to mongoose!');
 });
 
-/* bodyParser allows Express to handle readng data from form elements
- * urlencoded method tells bodyParser to extract data from the <form> element
- * and add that data to the body property of the request object
+
+/* built in express bodyParser from version 4.16+
+ * urlencoded method extracts data from the <form> element
+ * and adds that data to the body property of the request object
  */
 
-app.use(bodyParser.urlencoded({ extended: true }));
-/* need to add this second line to prevent post req.body from being a nested obj
- * then associated POST request from form can have content-type application/json
- */
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json(), cookieParser(), express.urlencoded({ extended: false }));
 
 // serve static files from assets folder
-app.use('/assets/', express.static(path.resolve(__dirname, '../assets')));
 
-// route handlers
-app.get('/secret', (req, res) => {
-  // check for cookie before rendering
-  console.log('req.cookies ', req.cookies)
-  // checks if user has a token 'admin' in their cookies before rendering /secret page
-  if (req.cookies.token === 'admin') {
-    res.status(200).sendFile(path.join(__dirname, '../views/secret.html'));
-  } else {
-    res.status(403).send('You must be signed in to view this page');
-  }
-});
+// ! I still don't really understand what this views page is for
+app.use('/assets/', express.static(path.resolve(__dirname, '../assets')));
+app.use('/views/', express.static(path.resolve(__dirname, '../views')));
+
+// app.use(express.static('../assets/'), express.static('views'));
+
+/*  route handlers */
 
 // route to taskController.postTask middleware in taskController file
-app.post('/tasks/add', (req, res) => {
-  console.log('post req.body', req.body);
-  // got a nested object need to get one level deeper
-  Task.create(req.body, (err, results) => {
-    console.log('results within Task.create ', results);
-    if (err) console.log(err);
-    // res.redirect('/secret');
-  });
-});
+app.post('/tasks/add', TaskController.postTask);
 
 // route to taskController.deleteTask middleware in taskController file
-app.delete('/tasks/:id', (req, res) => {
-  console.log('delete req.params ', req.params.id)
-  Task.findByIdAndDelete(req.params.id, (err, results) => {
-    if (err) console.log(err);
-  });
+app.delete('/tasks/:id', TaskController.deleteTask);
+
+// redirects user to signin page after submitting the login form
+app.post('/signin', AuthController.verifyUser, AuthController.setCookie, AuthController.userLogin);
+
+app.get('/secret', AuthController.checkCookie, (req, res) => {
+  // check for cookie via middleware before rendering
+  // ! not sure if I should render this here or in my authController?
+  res.status(200).sendFile(path.join(__dirname, '../views/secret.html'));
 });
 
 // route to taskController.getTasks middleware to return tasks from db
-app.get('/tasks', (req, res) => {
-  console.log('made it to get in server.js!');
-  Task.find((err, results) => {
-    console.log('console logging results ', results);
-    if (err) return console.log(err);
-    res.send(results);
-  });
-});
-
-// redirects user to signin page after submitting the login form
-app.post('/signin', (req, res) => {
-  console.log('req.boy signin form ', req.body);
-  if (req.body.user === 'codesmith' && req.body.pass === 'ilovetesting') {
-    res.cookie('token', 'admin');
-    res.redirect('/secret');
-  } else {
-    // find way to display "incorrect login info" on screen
-    res.status(403).send('unsuccessful login attempt');
-  }
-});
+app.get('/tasks', TaskController.getTasks);
 
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../views/index.html'));
