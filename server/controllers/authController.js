425bab7910679d53fff1459d67b1f07c04d7cb7d
ibe.eliot <41167569+ibeeliot@users.noreply.github.com>
2020-04-11 16:04:41 -0700

    attempting authorizating

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,22 +1,6 @@
-module.exports = {
-  setCookie: (req, res, next) => {
-    if (req.body.user === 'codesmith' && req.body.pass === 'ilovetesting') {
-      res.cookie('token', 'admin');
-      return next();
-    } else 'unsuccessful login attempt';
-  },
-  deleteCheck: (req, res, next) => {
-    text = 'SELECT * from messages where id = $1';
-    const { id } = req.params;
-    values = [id];
-    db.query(text, values)
-      .then(message => {
-        if (!req.cookie.pass || req.cookie.pass !== message.rows.password) {
-          res.status(401); // means unauthorized
-        } else {
-          return next();
-        }
-      })
-      .catch(err => console.log(err));
-  }
+const authController = {};
+authController.setCookie = (req, res, next) => {
+  res.cookie = `pass=${req.body.pass}`;
+  return next();
 };
+module.exports = authController;
