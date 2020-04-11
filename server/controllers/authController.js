b3fe30827fa2eeb88ce7fca5dbf1cfdc2716d478
andieritter <58995450+andieritter@users.noreply.github.com>
2020-04-11 16:32:22 -0700

    assessment retake not finsihed, working on auth portion.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,17 @@
-module.exports = {
+const authController = {};
 
+const user = 'codesmith';
+const password = 'ilovetesting';
 
-};
+authController.verifyUser = (req, res, next) => {
+  if (req.body.user === user && req.body.password === password) {
+    // set cookie
+    res.cookie('token', 'admin');
+    return next();
+  } else {
+    console.log('unsuccessful login attempt');
+    next();
+  }
+}
+
+module.exports = authController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,33 @@
+const { Pool } = require('pg');
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://rsaalztr:INS0Y-TXBj5TpiS-99UMVDXxQJcnfuky@drona.db.elephantsql.com:5432/rsaalztr';
+
+const pool = new Pool({
+  connectionString: myURI
+})
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
+
+/* POSTGRESQL TABLE SCHEMA
+CREATE TABLE Task (
+	_id serial NOT NULL,
+	item VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT now(),
+	CONSTRAINT Task_pk PRIMARY KEY (_id)
+) WITH (
+  OIDS=FALSE
+);
+*/
 
 
-
-
-module.exports = null; // <-- export your model
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  }
+}; // <-- export your model
