10ad6cc49e78169c78653194333fb2e2735ace48
Noah King <noah.h.king@gmail.com>
2020-04-11 16:40:45 -0700

    completed but not totally functional

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,38 @@
-module.exports = {
+const Task = require("../models/TaskModel");
 
+taskController = {};
 
+taskController.postTasks = (req, res, next) => {
+  const task = {
+    task: req.body.task,
+  };
+  Task.create(task, (err, result) => {
+    if (err) return next(err);
+    else {
+      res.locals.newTask = result;
+      return next();
+    }
+  });
 };
+taskController.getTasks = (req, res, next) => {
+  Task.find({})
+    .exec()
+    .then((results) => {
+      res.locals.tasks = results;
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
+taskController.deleteTasks = (req, res, next) => {
+  const itemId = req.body.id;
+  Task.deleteOne({ _id: itemId }, (err) => {
+    if (err) return next(err);
+    else {
+      res.locals.bool = true;
+      return next();
+    }
+  });
+};
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,41 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const mongoose = require("mongoose");
+
+const myURI =
+  "mongodb+srv://noahking:pipking1@cluster0-7jups.mongodb.net/test?retryWrites=true&w=majority";
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "Task",
+  })
+
+  .then(() => {
+    console.log("Connected to mongo...");
+  })
+  .catch((err) => console.log(err));
+
+const Schema = mongoose.Schema;
+
+const Tasks = new Schema({
+  item: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Task = mongoose.model("Task", Tasks);
 
 
 
-
-module.exports = null; // <-- export your model
+module.exports = Task; // <-- export your model
