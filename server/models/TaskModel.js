4aea4778848871544b9bb741d3533f796f256e2f
Jessikee Campbell-Walker <jessikeecw@gmail.com>
2020-04-11 14:02:59 -0700

    completed the task model setup

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,34 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = `mongodb+srv://${process.env.PASSWORD}@tasks-nxs5v.mongodb.net/test?retryWrites=true&w=majority`;
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
 
+//connects mongoose to the mongodb and returns a promise
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'Tasks',
+  })
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((err) => {
+    console.log(`Error: ${err}`);
+  });
 
+//defines the model for the task
+const schema = new mongoose.Schema({
+  item: String,
+  created_at: { type: Date, default: Date.now },
+});
+const Task = mongoose.model('Task', schema);
 
-
-module.exports = null; // <-- export your model
+module.exports = Task; // <-- export your model
