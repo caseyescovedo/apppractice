bcbb1ec81b6a87cff18204d42f13246956ac4b69
ibe.eliot <41167569+ibeeliot@users.noreply.github.com>
2020-04-11 16:31:53 -0700

    updated some trace annotations

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,15 @@
 const authController = {};
 authController.setCookie = (req, res, next) => {
-  res.cookie = `pass=${req.body.pass}`;
+  res.cookie = `token=admin`;
   return next();
 };
+// WAS STILL WORKING ON VALIDATION
+// if (user === 'codesmith' && pass === 'ilovetesting') {
+//     next();
+//   } else {
+//     //   if user and password didn't match, just throw an error message immediately
+//     return res.status(401).send('unsuccessful login attempt');
+//   }
 
 authController.validate = () => {};
 module.exports = authController;
