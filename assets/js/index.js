2b587bb2366cbbdc2e0dacc78805b8f8915cd8b6
Jessikee Campbell-Walker <jessikeecw@gmail.com>
2020-04-11 23:58:34 -0700

    simplified  and restructure getting and posting task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+const tasks = document.getElementById('task-list');
 //when the page loads, it will invoke the onload function
 onload();
 
@@ -23,7 +24,7 @@ function postTask() {
   //   alert('Task is empty');
   // } else {
   console.log('posting Task');
-  fetch('/post', {
+  fetch('/items', {
     method: 'POST',
     headers: {
       'Content-Type': 'Application/JSON',
@@ -39,9 +40,10 @@ function postTask() {
 //function to get all of the task
 //When the button is clicked to get tasks, all tasks from the database should be displayed as list items in the #task-list element
 function getTask() {
-  fetch('/get')
+  fetch('/items')
     .then((res) => res.json())
     .then((res) => {
+      // console.log(res);
       const taskList = document.getElementById('task-list');
       //sets the task list to an empty string
       taskList.innerHTML = '';
@@ -49,41 +51,44 @@ function getTask() {
       for (let i = 0; i < res.length; i++) {
         //creates a list item in the task list
         let oneTask = document.createElement('li');
-        //give the task an ID and context
-        oneTask.id = res[i]._id;
-        oneTask.innerHTML = res[i].item;
-
         //These list items should display the task item followed by a button (inside the list item) with a class of remove and display an X.
         //creates a delete button ans listens for when the button has been clicked
         const deleteBttn = document.createElement('button');
         deleteBttn.textContent = 'X';
-        //Clicking on any list item's X button should remove the item from the list (immediately) and delete the task from the database
-        deleteBttn.addEventListener('click', () => {
-          deleteTask(res[i]._id);
-        });
+        //adds a class to the delete button
+        deleteBttn.classList.add('remove');
+        //connects the id from the response to the delete button
+        deleteBttn.setAttribute('id', res[i]._id);
+        // console.log(res[i]._id);
+
+        //sets the inside of the task item to have the to do
+        oneTask.textContent = res[i].item;
         //adds the task item to the task item list
         taskList.appendChild(oneTask);
         // attaches the task item to the delete button
-        taskList.appendChild(deleteBttn);
+        oneTask.appendChild(deleteBttn);
+
+        //Clicking on any list item's X button should remove the item from the list (immediately) and delete the task from the database
+        deleteBttn.addEventListener('click', (e) => {
+          // has all of the delete task funcitonallity
+          //removes the task item from the task list which will  also remove the delete button
+          tasks.removeChild(e.target.parentElement);
+          // console.log(_id);
+          console.log(res);
+          //grabs the task by its id
+          fetch('/items', {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'Application/JSON',
+            },
+            body: JSON.stringify({ _id: res[i]._id }),
+          }).catch((err) => {
+            console.error(`Error in the delete message from the index.js ${err}`);
+          });
+        });
       }
     })
     .catch((err) => {
-      console.error('Error in the get message from the index.js', err);
+      console.error(`Error in the get message from the index.js: ${err}`);
     });
-}
-//Clicking on any list item's X button should remove the item from the list (immediately) and delete the task from the database
-function deleteTask(id) {
-  const deleteTask = document.getElementById(id);
-  const tasks = document.getElementById('task-list');
-  //removes the task item from the task list
-  tasks.removeChild(deleteTask);
-  //grabs the task by its id
-  fetch(`/delete/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'Application/JSON',
-    },
-  }).catch((err) => {
-    console.error('Error in the delete message from the index.js', err);
-  });
 }
