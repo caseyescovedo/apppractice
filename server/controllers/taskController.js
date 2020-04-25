a5f5d7778e9b38252d779c80fcae86eb16840fd6
ajlee12 <rach.on@gmail.com>
2020-04-25 15:38:50 -0700

    Finished basic functionalities before authen.

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,77 @@
-module.exports = {
+const pool = require('../models/TaskModel.js');
 
+const taskController = {};
+
+// create a new item in the database
+taskController.postTask = (req, res, next) => {
+  // console.log('what is the pool?:', pool);
+  // console.log('(controller.postTask) req.body:', req.body);
+
+  const params = [req.body.item];
+  const insertQuery = `
+    INSERT INTO Tasks(item)
+    VALUES ($1);
+  `;
+
+  pool.query(insertQuery, params)
+  // .then(res => res.json())
+  .then(postedTask => {
+    res.locals.postedTask = req.body.item;
+    return next();
+  })
+  .catch(err => {
+    // console.log('err in postTask:', err);
+    return next({
+      log: 'Error while inserting to PG db.',
+      message: err,
+    })
+  })
 
 };
+
+// retrieve all items from the database and send it back to the client as JSON
+taskController.getTasks = (req, res, next) => {
+  const getAllTasksQuery = `
+    SELECT * from Tasks;
+  `;
+
+  pool.query(getAllTasksQuery)
+  .then(allTasks => {
+    // Successful query will return the tasks in docs.rows (an array).
+    // console.log('(controller.getTasks) allTasks:', allTasks.rows);
+    res.locals.allTasks = allTasks.rows;
+    return next();
+  })
+  .catch(err => {
+    return next({
+      log: 'Error while getting tasks from PG db.',
+      message: err,
+    })
+  })
+};
+
+// should find items in the db based on ID (created_at?) and delete that item if it exists
+taskController.deleteTask = (req, res, next) => {
+  const params = [req.params.id];
+  const deleteQuery = `
+    DELETE FROM Tasks
+    WHERE _id=($1)
+  `;
+
+  pool.query(deleteQuery, params)
+  .then(deletedTask => {
+    console.log('(controller.deleteTask) task deletion successful')
+    res.locals.deletedItem = deletedTask;
+    return next();
+  })
+  .catch(err => {
+    return next({
+      log: 'Error while deleting a task from PG db.',
+      message: err,
+    })
+  })
+
+};
+
+
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,18 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://ixzghwnl:trHsEIXxYc5wB0gcITG-5aNhPLCrMyxw@drona.db.elephantsql.com:5432/ixzghwnl';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  connectionString: URI,
+});
 
+pool.connect();
 
-module.exports = null; // <-- export your model
+module.exports = pool; // <-- export your model
