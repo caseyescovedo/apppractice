4bfa2cd756ed8a943f573faa062a161102d80b1f
Sieun J <sjang@Sieuns-MacBook-Pro.local>
2020-04-25 14:32:00 -0700

    app assessement

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,19 @@
 module.exports = {
+  verifyUser: (req, res, next) => {
+    const { user, pass } = req.body
+    if(user === 'codesmith' && pass === 'ilovetesting') {
+      res.cookie('token', 'admin', {
+        maxAge: 360000,
+        httpOnly: true,
+      });
+      return next()
+    }
+    else res.json('unsuccessful login attempt')
+  },
 
-
+  isLoggedIn: (req, res, next) => {
+    const cookie = req.cookies.token;
+    if(cookie === 'admin') return next()
+    else res.json('You must be signed in to view this page')
+  }
 };
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,20 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://osyvjgiy:3Q-madXKkPZXXyYEnLyoQYDpUJLkdStZ@drona.db.elephantsql.com:5432/osyvjgiy';
+const { Pool } = require('pg');
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+const pool = new Pool({
+  connectionString: URI
+})
 
-
-
-module.exports = null; // <-- export your model
+module.exports = {
+  query: (text, params) => {
+    console.log(`executed query ${text}`);
+    return pool.query(text, params)
+  }
+} 
