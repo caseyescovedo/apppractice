e550a7511ab6828f7cf709d6c18ca7e6a0c8193f
twastell <tbwastell@gmail.com>
2020-04-25 16:38:05 -0700

    still a long way to go....

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,31 @@
-module.exports = {
+const Task = require('../models/TaskModel');
 
+const taskController = {
 
-};
+    postTask (req, res, next) {
+        Task.create({task: req.body.task, created_at: new Date() }),
+        (err, task) => {
+            if (err) return next(err)
+            res.locals.task = task;
+            next();
+        }
+    },
+
+    getTasks (req, res, next) {
+        Task.find(), (err, tasks) => {
+            res.locals.tasks = tasks;
+            next();
+        }
+        
+    },
+
+    deleteTask (req, res, next) {
+        Task.findOneAndDelete({_id: req.body._id}, (err, task) => {
+            if (err) return next(err)
+            if(!task) return next('Task not found')
+            next();
+        })
+    }
+}
+
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,18 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
 const myURI = '';
+const mongoose = require('mongoose')
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
 
+const taskSchema = new mongoose.Schema({
+    item: {type: string, require: true, unique: true},
+    created_at: {type: string }
+})
 
+const Task = mongoose.model('task', taskSchema) 
 
-
-module.exports = null; // <-- export your model
+module.exports = Task; // <-- export your model
