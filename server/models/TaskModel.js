602c6c0461839b15837f22795df14cd781bc48ac
VaughnHartling <Voonhartking@gmail.com>
2020-04-25 13:14:23 -0700

    Connected to database.

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,11 +1,11 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://tglhatdt:d8yyeKaycQUpb35j6GG3MNsEKkGkuKxT@drona.db.elephantsql.com:5432/tglhatdt';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
 
 
