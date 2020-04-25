e9e60af3c7f1f158858ebc3486039500a2916d50
Alexander Infante <alexinfante17@gmail.com>
2020-04-25 16:37:15 -0700

    Codesmith Assessment Part 3, incomplete as of 4.25.20 @ 4:38P

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,15 @@
-module.exports = {
+const authController = {};
 
+// login
+authController.login = (req, res, next) => {
+  const usernameAttempt = req.body.user;
+  const passwordAttempt = req.body.pass;
 
-};
+  if (usernameAttempt === 'codesmith' && passwordAttempt === 'ilovetesting') {
+    next();
+  } else res.status(200).send('unsuccessful login attempt');
+}
+
+// cookie
+
+module.exports = authController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,25 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'mongodb+srv://Alex:Alex@cluster0-mhly7.mongodb.net/test?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
-// UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+// Connecting Database
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'DBtest',
+})
 
 
+// Model
+const taskSchema = new mongoose.Schema ({
+  item: {type: String, required: true},
+  created_at: Date,
+});
 
+const Task = mongoose.model('task', taskSchema)
 
-module.exports = null; // <-- export your model
+// Export
+module.exports = Task; // <-- export your model
