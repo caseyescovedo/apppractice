d48dde18b72fd8ac96a3a63530b55d33f16a85c3
Fan <fanny.shao18@gmail.com>
2020-05-16 16:26:00 -0400

    crud assessment

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,24 @@
+const { Pool } = require('pg')
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://irdbqpcc:4_EdzNFw0Ynvi4YAjr120ZFlzhEGjuwE@isilo.db.elephantsql.com:5432/irdbqpcc';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
+
+// iniitialize pool and client with connection string
+const pool = new Pool({
+  connectionString: URI
+})
+
+pool.connect()
 
 
 
 
-module.exports = null; // <-- export your model
+
+module.exports = pool;
+// module.exports = null; // <-- export your model
