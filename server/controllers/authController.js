76986f3d2ff003f18c969c5232d67a32a0de1bd6
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-11 23:48:33 +0100

    completed auth controller with user authentication and annonymous authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,35 @@
 module.exports = {
-
-
+  authenticateUser: async (req, res, next) => {
+    const token = req.headers['authorization'].replace('Bearer ', '');
+    if (!token || token !== 'admin') {
+      return next({ error: 'You must be signed in to view this page' });
+    }
+    return next();
+  },
+  authenticateAnnonymous: async (req, res, next) => {
+    // different appraoch from client-side
+    const cookies = req.headers.cookie;
+    const allThs = cookies
+      .split(';')
+      .filter((cookie) => cookie.startsWith(` token=`));
+    if (allThs.length) {
+      const token = allThs[0].replace(' token=', '');
+      if (!token || token !== 'admin') {
+        return next({ error: 'You must be signed in to view this page' });
+      }
+      return next();
+    }
+    return next({ error: 'You must be signed in to view this page' });
+  },
+  login: async (req, res, next) => {
+    if (!req.body.user || !req.body.pass) {
+      return next({ log: 'unsuccessful login attempt' });
+    }
+    if (req.body.user === 'codesmith' && req.body.pass === 'ilovetesting') {
+      res.locals.token = 'admin';
+    } else {
+      return next({ log: 'unsuccessful login attempt' });
+    }
+    return next();
+  },
 };
