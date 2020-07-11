9355f10e5f85bf94b628a73dc5c4f8aee7432054
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-11 23:56:33 +0100

    refined and completed controllers for CRUD routes

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,63 @@
+const { Tasks } = require('../models/TaskModel');
+
+async function allTasks() {
+  try {
+    const allTasks = await Tasks.find({}, { __v: 0 });
+    return allTasks;
+  } catch (err) {
+    return [];
+  }
+}
+
 module.exports = {
-
-
+  addTaskTest: async () => {
+    try {
+      const tasks = await new Tasks({
+        item: 'Completing graduation assessment',
+      }).save();
+      console.log('Created Tasks :: ', tasks);
+      return next();
+    } catch (err) {
+      console.log('Error creating tasks :: ', err);
+      return next({ error: err });
+    }
+  },
+  postTask: async (req, res, next) => {
+    const { item } = req.body;
+    if (!item) {
+      return next({ log: 'No task provided' });
+    }
+    try {
+      await new Tasks({
+        item,
+      }).save();
+      const tasks = await allTasks();
+      res.locals.tasks = tasks;
+      return next();
+    } catch (err) {
+      console.log('Error creating task :: ', err);
+      return next({ error: err });
+    }
+  },
+  getTasks: async (req, res, next) => {
+    const tasks = await allTasks();
+    res.locals.tasks = tasks;
+    return next();
+  },
+  deleteTask: async (req, res, next) => {
+    const { _id } = req.params;
+    if (!_id) {
+      return next({ log: 'Please provide the id of task to be deleted' });
+    }
+    try {
+      await Tasks.findByIdAndRemove({ _id });
+      const tasks = await allTasks();
+      res.locals.tasks = tasks;
+      return next();
+    } catch (err) {
+      res.locals.tasks = [];
+      console.log('Error deleting task :: ', err);
+      return next({ error: err });
+    }
+  },
 };
