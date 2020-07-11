84aa51d391283fa61ed4533a8c2dc5c925d0d20f
Veronica Stork <zealous.anemone@gmail.com>
2020-07-11 18:52:51 -0400

    User can add to list and view list. Delete doesn't work yet.

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,16 @@
+const { Pool } = require("pg");
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI =
+  "postgres://rsobxnvb:QIoWPnfbYAVxwtHxInOTOudfXK280EUg@ruby.db.elephantsql.com:5432/rsobxnvb";
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+const pool = new Pool({
+  connectionString: URI,
+});
 
-
-
-module.exports = null; // <-- export your model
+module.exports = pool; // <-- export your model
