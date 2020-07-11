ee44c59fc60cae74553b832ef582f3644df0d696
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-11 23:47:54 +0100

    completed app routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,11 @@
 const appRoutes = require('express').Router();
-const auth = require('../controllers/authController');
 const tasks = require('../controllers/taskController');
 
 appRoutes.post('/', tasks.postTask, (req, res) => {
   return res.status(200).json({ tasks: res.locals.tasks });
 });
 
-appRoutes.delete('/', tasks.deleteTask, (req, res) => {
+appRoutes.delete('/:_id', tasks.deleteTask, (req, res) => {
   return res.status(200).json({ tasks: res.locals.tasks });
 });
 
