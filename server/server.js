782859dcd932be647755c4b44416277e6ad64e14
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-11 23:07:01 +0100

    refined server entry point

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 require('dotenv').config();
 
 const { connectDB } = require('./models/TaskModel');
+const routes = require('./routes');
+const auth = require('./controllers/authController');
 
 const app = express();
 
@@ -22,7 +24,17 @@ app.get('/secret', (req, res) => {
   res.sendFile(path.resolve('./views', 'secret.html'));
 });
 
-app.use(function (err, req, res, next) {});
+app.use('/tasks', routes);
+app.post('/signin', auth.login, (req, res, next) => {
+  res.status(200).json({ status: true, token: res.locals.token });
+});
+
+app.use(function (err, req, res, next) {
+  if (err && err.log) {
+    return res.status(500).json({ ...err, status: false });
+  }
+  res.status(500).json({ err: 'Error occured', message: err });
+});
 
 app.listen(port, () => {
   console.log(`App Assessment Mod 0 Backend API listening on ${port}`);
