b8770646b5a8c391c08eb149a615eb8ec6244b03
Tariq Khan <digitalgnomes@gmail.com>
2020-07-11 15:57:06 -0700

    Submission for Graduation Assessment CRUD App

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,27 @@
-module.exports = {
+const { Task, db } = require('../models/TaskModel');
 
-
+const postTask = async (req, res) => {
+  const { taskData } = req.body;
+  const todoTask = new Task({
+    item: taskData,
+  });
+  try {
+    await todoTask.save();
+    res.redirect('/secret');
+  } catch (err) {
+    res.redirect('/');
+  }
 };
+
+const getTasks = async (req, res) => {
+  await Task.find({}, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('result', result);
+      res.json({ result });
+    }
+  });
+};
+
+module.exports = { postTask, getTasks };
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,38 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = process.env.DATABASE_URI;
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
 
+// connect to db
+mongoose.connect(myURI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
 
+// connect to db and store connection to a variable
+const db = mongoose.connection;
+// db connection error handling
+db.on('error', console.error.bind(console, 'DB connection error'));
 
+// successful db connection
+db.once('open', () => {
+  console.log('DB Connection Successful!');
+});
 
-module.exports = null; // <-- export your model
+const TaskSchema = new mongoose.Schema(
+  {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Task = mongoose.model('Task', TaskSchema);
+
+module.exports = { Task, db };
