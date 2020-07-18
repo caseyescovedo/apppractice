658c40b8a63952382f73f51157e060ab6d8493fc
Katty <kpolyak1@binghamton.edu>
2020-07-18 15:43:42 -0700

    finish getTasks on page load of secret.html

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+/**
+ * Gets all the tasks in the database and displays them on the page with a button to remove
+ */
+const getTasks = () => {
+  let list = document.getElementById('task-list');
+  console.log(list);
+  // clear out the list every time
+  fetch('/tasks')
+    .then((response) => response.json())
+    .then((items) => {
+      items.forEach((task) => {
+        // create a button
+        const button = document.createElement('button');
+        button.innerHTML = 'X';
+        button.id = task.id;
+        button.className = 'remove';
+        button.onclick = () => deleteTask(button.id);
+
+        const newItem = document.createElement('li');
+        newItem.innerText = task.item;
+        newItem.id = task.id;
+
+        newItem.appendChild(button);
+        // add our item to the list
+        list.appendChild(newItem);
+      });
+    })
+    .catch((err) => console.log(err));
+  // loop through the fetch response and set inner html for each item
+};
+
+const postTasks = () => {};
+
+const deleteTask = () => {
+  // send a fetch with request type delete
+};
+
+getTasks();
