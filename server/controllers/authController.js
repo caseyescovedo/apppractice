edfc34814b4b93370f602285df2c9848371c43b8
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-07-18 16:15:18 -0700

    finish app assessment retake

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,23 @@
-module.exports = {
+const authController = {};
 
-
+authController.checkCredentials = (req, res, next) => {
+  const { user, pass} = req.body;
+  if(user == 'codesmith' && pass == 'ilovetesting'){
+    res.cookie('token', 'admin');
+    return next();
+  } else {
+    res.cookie('token', 'no-access')
+    return next();
+  }
 };
+
+authController.checkCookies = (req, res, next) => {
+  if(req.cookies.token) {
+    const { token } = req.cookies;
+    if(token == 'admin'){
+      return next();
+    } else res.redirect('/failedAuth');
+  }
+};
+
+module.exports = authController;
