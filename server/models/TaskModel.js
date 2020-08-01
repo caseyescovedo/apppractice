f3f3714694f6fcd591679cb1adfaaa7c397d0768
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-08-01 13:02:35 -0700

    update

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,8 +1,8 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'mongodb+srv://anthony:asdasd@cluster0.46p4v.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
