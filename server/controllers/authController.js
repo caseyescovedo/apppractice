af8a06aa19f7a9a027fb2c03858768522749fadc
mjmiguel <migueljmiller@gmail.com>
2020-08-01 16:27:10 -0700

    complete task manager assessment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,44 @@
+// user table is stored in the same db as task table
+const db = require('../models/TaskModel');
+
 module.exports = {
 
+  // verify user. passwords are stored as PLAIN TEXT in db <-- BAD
+  verifyUser: function(req, res, next) {
+    console.log('req cookie ', req.cookies);
+    const { user, pass } = req.body;
+    if (!user || !pass) return next();
+    const query = `SELECT * FROM users WHERE username=$1`
+    const values = [user];
+    db.query(query, values, (err, foundUser) => {
+      if (err) return next({ location: 'verifyUser', log: err });
+      // if user not found in db
+      if (foundUser.rows.length === 0) return next();
+      // if supplied password doesnt match db password
+      if (foundUser.rows[0].password !== pass) return next();
+      // if everything matches, store data
+      res.locals.data = foundUser.rows[0];
+      return next();
+    });
+  },
 
+  // check for cookie or set cookie
+  setCookie: function(req, res, next) {
+    console.log('in set cookie');
+    console.log('req cookie ', req.cookies);
+    if (!res.locals.data) return next();
+    res.cookie('token', 'admin')
+    return next();
+  },
+
+  // check for cookie, pass boolean along
+  checkCookie: function(req, res, next) {
+    const { token } = req.cookies;
+    if (!token || token !== 'admin') {
+      res.locals.cookie = false;
+      return next();
+    }
+    res.locals.cookie = true;
+    return next();
+  }
 };
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,23 @@
+const { Pool } = require('pg');
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://hzjsojcz:u71iWGYBwVdkP38KmAQwyB2tMFDt23tX@ruby.db.elephantsql.com:5432/hzjsojcz';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
+
+const pool = new Pool({
+  connectionString: myURI,
+});
 
 
 
-
-module.exports = null; // <-- export your model
+module.exports = { 
+  query: function (text, params, callback) {
+    console.log(`Executing query: ${text}`);
+    return pool.query(text, params, callback);
+  }
+};
