b009b0b0a11194e327cf4afff49ff5ccf24f4689
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2020-08-01 14:15:44 -0700

    db schema error fixed

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -3,15 +3,16 @@ const mongoose = require('mongoose');
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
 const myURI = '';
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI;
 
-mongoose.connect(myURI, { useNewUrlParser: true }, (err) => {
-  if (err) console.log(err);
-  else console.log(`db connected!`);
+mongoose.connect(myURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else console.log(`db connected!`);
 });
 
 const taskSchema = new mongoose.Schema({
-  item: { String, required: true },
+  item: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
 });
 
