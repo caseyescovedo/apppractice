6f2585bc3c7a08fe3f827b70e0d1ae0b29fd73a2
Nicolas Pita <nicolaspita@NMBP.home>
2020-08-01 16:32:16 -0700

    complete assessment part 2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,12 @@
-module.exports = {
+const express = require('express');
+const session = require('express-session');
+const authController = {};
 
-
+authController.validateUser = (req, res, next) => {
+  // use this to validate cookies
+  if (req.session.cookie.secret) return next();
+  req.session.cookie.secret = 'shhhhh';
+  return next();
 };
+
+module.exports = authController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,27 @@
+const mongoose = require('mongoose');
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'mongodb+srv://nicolaspita:rambler@cluster0.incrp.mongodb.net/Task?retryWrites=true&w=majority';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
 
+// connect to the data base
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to db...')
+  })
+  .catch((err) => console.log(err));
 
+// create Task model
+const taskModel = mongoose.Schema({
+  item: String,
+  created_at: { type: Date, default: Date.now }
+})
 
+const Task = mongoose.model('Task', taskModel);
 
-module.exports = null; // <-- export your model
+module.exports = Task; // <-- export your model
