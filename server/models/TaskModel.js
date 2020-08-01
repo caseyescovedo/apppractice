add57c7ef8386245780371130bfe2b28f9011ccb
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2020-08-01 13:18:55 -0700

    server set up

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,18 @@
+const mongoose = require('mongoose');
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
 const myURI = '';
-
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
-// UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+mongoose.connect(myURI, { useNewUrlParser: true }, (err) => {
+  if (err) console.log(err);
+  else console.log(`db connected!`);
+});
 
+const taskSchema = new mongoose.Schema({
+  item: { String, required: true },
+  created_at: { type: Date, default: Date.now },
+});
 
-
-
-module.exports = null; // <-- export your model
+module.exports = mongoose.model('Task', taskSchema);
