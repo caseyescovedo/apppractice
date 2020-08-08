6810ac2ee6c11d2d96b3ad869ab104a5c57fb3e2
Wyatt Bell <wyattbell@macbook-pro-315.lan>
2020-08-08 17:31:48 -0400

    app assessment as complete as ever

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,36 @@
-module.exports = {
+const Task = require('../models/TaskModel.js');
 
+const taskController = {};
 
-};
+taskController.postTask = (req, res, next) => {
+  const { item } = req.body;
+
+  Task.create({ item })
+    .then(task => {
+      res.locals.data = task;
+      return next();
+    })
+    .catch(err => next(err));
+}
+
+taskController.getTasks = (req, res, next) => {
+  Task.find({}).exec()
+    .then(tasks => {
+      res.locals.data = tasks;
+      return next();
+    })
+    .catch(err => next(err));
+}
+
+taskController.deleteTask = (req, res, next) => {
+  const { id } = req.params;
+
+  Task.findByIdAndDelete(id).exec()
+    .then(task => {
+      res.locals.data = task;
+      return next();
+    })
+    .catch(err => next(err));
+}
+
+module.exports = taskController;
