5b835a6360ba729c86c1ec545e96b3e706541be4
Cameron Fitz <hellocameronfitz@gmail.com>
2020-08-08 16:23:48 -0400

    Made good progress!

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,37 @@
+const Task = require('../models/TaskModel');
+
 module.exports = {
+	postTask: async function(req, res, next) {
+		try {
+			const task = await Task.create(req.body);
+			// const tasks = await task.find({});
+			// res.locals.tasks = tasks;
+			return next();
+		} catch (error) {
+			console.log(error);
+			return next(error);
+		}
+	},
+	getTasks: async function(res, res, next) {
+		try {
+			const tasks = await Task.find({});
+			res.locals.tasks = tasks;
+			return next();
+		} catch (error) {
+			console.log(error);
+			return next(error);
+		}
+	},
+	deleteTask: async function(req, res, next) {
+		try {
+			let deleted = await Task.findOneAndDelete({
+				_id: req.params.id
+			});
 
-
+			return next();
+		} catch (error) {
+			console.log(error);
+			return next(error);
+		}
+	}
 };
