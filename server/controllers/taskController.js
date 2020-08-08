554dcf98a0636887e26f6b2dc427e40a07d8930d
Jehovany Cruz <jehovany@Jehovanys-Air.fios-router.home>
2020-08-08 16:32:24 -0400

    jeho test

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,45 @@
-module.exports = {
+const db = require('../models/TaskModel.js')
+const taskController = {};
+
+taskController.postTask = (req,res,next) => {
+    const item = req.body.item
+    const created_at = req.body.created_at
+    console.log(req.body.item)
+    console.log(req.body.created_at)
+    const addTask = `INSERT INTO Task(item, data_created)
+    VALUES($1,$2);`
+    db.query(addTask,[item, created_at])
+    .then(() => {
+        return next();
+    })
+    .catch(err => next({
+        status: 500,
+        log: 'this came from task controller post'}))
+}
+
+taskController.getTasks = (req,res,next) => {
+    const getTasks = `SELECT * FROM Task`
+    db.query(getTasks)
+    .then((result) => {
+        res.locals.tasks = result.rows
+        return next();
+    })
+    .catch(err => next({
+        status: 500,
+        log: 'this is broken, gettasks'}))
+}
+
+taskController.deleteTask = (req,res,next) => {
+    const deleteTask = `DELETE FROM task
+    WHERE id=$1;`
+    db.query(deleteTask, [req.params.id])
+    .then(() => {
+        return next();
+    })
+    .catch(err => next({
+        status: 500,
+        log: 'this came from delete task'}))
+}
 
 
-};
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,16 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
-
-// UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const {Pool} = require('pg')
+const myURI = 'postgres://yvuqerts:YKBJ1z1JsA3or2tfdn36C9TQBi3LMTrs@rajje.db.elephantsql.com:5432/yvuqerts';
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+const pool = new Pool ({
+    connectionString: myURI
+});
 
-
-
-module.exports = null; // <-- export your model
+module.exports = {
+    query: (text, params, callback) => {
+        return pool.query(text,params,callback)
+    }
+};
