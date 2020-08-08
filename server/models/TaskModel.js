aaa71f34e3673ff9ef2c4720670bc09a1705a929
Guest <Guest@Mac.local>
2020-08-08 16:01:14 -0400

    added

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,25 @@
+const mongoose = require('mongoose');
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI =
+  'mongodb+srv://win:winfordlin@users-jo7j2.mongodb.net/users?retryWrites=true&w=majority';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
 
+mongoose.connect(URI);
 
+mongoose.connection.once('open', () => {
+  console.log('Connected to Database');
+});
 
+const TaskSchema = new mongoose.Schema({
+  item: { type: String },
+  created_at: { type: Date, default: Date.now },
+});
 
-module.exports = null; // <-- export your model
+const Task = mongoose.model('task', TaskSchema);
+
+module.exports = Task; // <-- export your model
