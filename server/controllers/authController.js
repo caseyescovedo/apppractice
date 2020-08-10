76dd078b61cf25004264a138fdb41f9472c5bedb
VicBal <balanvic11235@gmail.com>
2020-08-09 21:51:48 -0400

    Added cookie parser and urlencoded ... Auth fixed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,15 +3,16 @@ const authController = {};
 authController.verifyUser = (req, res, next) => {
 	// user of codesmith and a pass of ilovetesting.
 	// redirect to the secret page route
-	if (!req.body.user !== 'codesmith' && req.body.pass !== 'ilovetesting') {
-		return res.status(400).json('Unsuccessful Login Attempt');
+	console.log(req.body)
+	if (req.body.user === 'codesmith' && req.body.pass === 'ilovetesting') {
+		return next();
 	}
-	return next();
+	return res.status(400).json('Unsuccessful Login Attempt');
 };
 
 authController.setCookie = (req, res, next) => {
 	// set a cookie with a key of token and a value of admind
-	res.cookie('token', admin);
+	res.cookie('token', 'admin');
 	// return to the next middleware
 	return next();
 };
@@ -19,7 +20,8 @@ authController.setCookie = (req, res, next) => {
 authController.verifyCookie = (req, res, next) => {
 	// check for the valid cookie
 	// If the cookie is not valid (or does not exist)
-	if (req.cookies !== 'admin' && !req.cookies) return res.status(400).json('You must be signed in to view this page');
+	if (req.cookies.token !== 'admin' || !req.cookies)
+		return res.status(400).json('You must be signed in to view this page');
 	return next();
 };
 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
 const taskController = require('./controllers/taskController');
-const authController = require('./controllers/authController')
+const authController = require('./controllers/authController');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('assets'));
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../views/index.html'));
-});
 
 app.get('/secret', authController.verifyCookie, (req, res) => {
 	res.sendFile(path.join(__dirname, '../views/secret.html'));
@@ -17,6 +16,10 @@ app.get('/secret', authController.verifyCookie, (req, res) => {
 
 app.post('/signin', authController.verifyUser, authController.setCookie, (req, res) => {
 	res.redirect('/secret');
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 app.get('/secret/tasks', taskController.getTasks);
