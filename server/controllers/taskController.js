e2197360808ef1f80e832f2f91164106d2a0ffca
Patrick Mullen <prmullen20@gmail.com>
2020-08-22 10:54:15 -0700

    working on db

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,54 @@
+const db = require('../models/TaskModel.js')
+
+
 module.exports = {
+  postTask: function(req, res, next) {
+    const {id, addItem} = req.body;
+    const query = `
+      INSERT INTO Task(ID, item)
+      VALUES ($1, $2);
+    `
+    const value = [id, addItem]
+    db.query(query, value)
+      .then((data)=>{
+        console.log(data)
+        res.sendStatus(200)
+      })
+      .catch((err)=>{
+        console.log('Error with postTask', err)
+        res.sendStatus(401)
+      })
+  },
+
+  getTasks: function(req, res, next) {
+    const query = `
+      SELECT * FROM "task"
+    `
+    db.query(query)
+      .then((data)=>{
+        console.log(data.rows);
+        res.locals.all = data.rows; 
+        res.status(200).json(res.locals.all)
+      })
+      .catch((err)=>{
+        console.log('Error with postTask', err)
+      })
+  },
+
+  deleteTask: function(req, res, next) {
+    const query = `
+      SELECT * FROM "task"
+    `
+    db.query(query)
+      .then((data)=>{
+        console.log(data.rows);
+        res.locals.all = data.rows; 
+        res.status(200).json(res.locals.all)
+      })
+      .catch((err)=>{
+        console.log('Error with postTask', err)
+      })
+  },
 
 
 };
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,30 @@
+const { Pool } = require('pg');
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://sjzkzwub:p8YCi3McOnxT8cDc6-_J1uzycsvto55x@lallah.db.elephantsql.com:5432/sjzkzwub';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+const pool = new Pool({
+  connectionString: URI,
+});
 
+const task = `
+CREATE TABLE Task (
+  ID VARCHAR,
+  item TEXT,
+  created_at TIME DEFAULT CURRENT_TIME
+);
+`
 
-
-module.exports = null; // <-- export your model
+// <-- export your model
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};; 
