dc4b16e8177d5ba92fd3380bd7a28b1e6e048247
Nick Kruckenberg <nkruckenberg@gmail.com>
2020-08-22 16:25:19 -0400

    CRUD functionality finished, but no authentication

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,43 @@
+/** ===== Require in PostgreSQL model ===== */
+const db = require('../models/TaskModel');
+
 module.exports = {
+  postTask: (req, res, next) => {
+    const queryString = 
+      `INSERT INTO Task (item, created_at)
+      VALUES ($1, $2)
+      RETURNING item, created_at;`
+    const queryValues = [req.body.item, req.body.created_at];
+    
+    db.query(queryString, queryValues)
+      .then(queryRes => {
+        res.locals.newTask = queryRes.rows;
+        return next();
+      })
+  },
 
+  getTasks: (req, res, next) => {
+    const queryString = 
+      `SELECT item, created_at
+      FROM Task;`
+    
+    db.query(queryString)
+      .then(queryRes => {
+        res.locals.tasks = queryRes.rows;
+        return next();
+      })
+  },
 
+  deleteTask: (req, res, next) => {
+    const queryString = 
+      `DELETE FROM Task
+      WHERE created_at = $1`;
+    const queryValues = [req.body.created_at];
+    
+    db.query(queryString, queryValues)
+      .then(queryRes => {
+        res.locals.deletedTask = queryRes.rows;
+        return next();
+      })
+  },
 };
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,23 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'postgres://kjfuwdud:wxBaI_MfUUnpG5SWjATjo0i06hFCHBwE@lallah.db.elephantsql.com:5432/kjfuwdud';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
+/** ===== Require Pool from node-postgress ===== */
+const { Pool } = require('pg');
 
+/** ===== Create new instance of Pool ===== */
+const pool = new Pool({
+  connectionString: URI,
+});
 
-
-module.exports = null; // <-- export your model
+/** ===== Export model ===== */
+module.exports = {
+  query: (queryString, params, cb) => {
+    return pool.query(queryString, params, cb);
+  },
+};
