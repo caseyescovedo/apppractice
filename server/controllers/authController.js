eb56966a0f4d4c26defff94c00d442d6db6be70d
Travis Frank <travis@travismfrank.com>
2020-08-22 14:56:20 -0400

    Completed signin / authentication functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,20 @@
 module.exports = {
 
+  verifyUser: (req, res, next) => {
+    if (req.body.user === 'codesmith' && req.body.pass === 'ilovetesting') {
+      return next();
+    }
+    return res.status(403).send('unsuccessful login attempt');
+  },
+
+  setAdminCookie: (req, res, next) => {
+    res.cookie('token', 'admin', {httpOnly: true});
+    return next();
+  },
+
+  verifyAdminCookie: (req, res, next) => {
+    if (req.cookies.token === 'admin') return next();
+    return res.status(403).send('You must be signed in to view this page');
+  }
 
 };
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const db = require('./models/TaskModel.js');
@@ -12,10 +13,20 @@ const app = express();
 
 // Parse body and cookies
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser());
 
 // Serve static files (CSS, JS, etc.)
 app.use(express.static(path.resolve(__dirname, '../assets')));
+
+// Signin
+app.post('/signin',
+  authController.verifyUser,
+  authController.setAdminCookie,
+  (req, res) => {
+    return res.redirect('/secret');
+  }
+);
 
 // Get all tasks
 app.get('/tasks', taskController.getTasks);
@@ -32,14 +43,17 @@ app.post('/tasks',
 app.delete('/tasks',
   taskController.deleteTasks,
   (req, res) => {
-    return res.sendStatus(200)
+    return res.sendStatus(200);
   }
 );
 
 // Todo Page
-app.get('/secret', (req, res) => {
-  return res.sendFile(path.resolve(__dirname, '../views/secret.html'));
-});
+app.get('/secret',
+  authController.verifyAdminCookie,
+  (req, res) => {
+    return res.sendFile(path.resolve(__dirname, '../views/secret.html'));
+  }
+);
 
 // Landing Page
 app.get('/', (req, res) => {
