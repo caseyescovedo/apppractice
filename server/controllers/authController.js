50771dce2779a5c57de7dc81ec57d30550127642
Heather Friedman <hfried25@gmail.com>
2020-08-22 16:23:06 -0400

    part 2 of grad assessment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,21 @@
-module.exports = {
+const db = require('../models/TaskModel.js');
 
+const authController = {};
 
+authController.validateUser = (req, res, next) => {
+  if (req.body.user === 'codesmith' && req.body.pass === 'ilovetesting') {
+    res.cookie('token', 'admin');
+  } else {
+    res.send('unsuccessful login attempt');
+  }
+  return next();
 };
+
+authController.checkUser = (req, res, next) => {
+  if (req.cookies.token === 'admin') {
+    return next();
+  } else {
+    res.send('You must be signed in to view this page');
+  }
+};
+module.exports = authController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,24 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const { Pool } = require('pg');
+const myURI =
+  'postgres://vbsjjoei:SUTEfxTFR_lRdWzU7SGkDBx4cdzcqmWP@lallah.db.elephantsql.com:5432/vbsjjoei';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
-// UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+//UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
+const URI = process.env.PG_URI || myURI;
 
+const pool = new Pool({
+  connectionString: URI,
+});
 
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+}; // <-- export your model
 
-
-module.exports = null; // <-- export your model
+//THIS IS THE QUERY I USED TO MAKE MY TABLE:
+//create table task (itemid serial primary key, item varchar not null, created_at time not null default current_timestamp);
