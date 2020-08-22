9c7cd95f9f30f7ec6e76909d5d369b1fc820c56b
Patrick Mullen <prmullen20@gmail.com>
2020-08-22 12:50:30 -0700

    completed all

--- server/server.js
+++ server/server.js
@@ -1,20 +1,31 @@
 const express = require('express')
 const path = require('path')
+const cookieParser = require('cookie-parser')
+
 const taskController = require('./controllers/taskController')
+const authController = require('./controllers/authController')
 
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser())
+
 app.use(express.static('./assets'))
 
 app.get('/secret', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../views/secret.html'))
+  if (req.cookies.token === 'admin') {
+    res.sendFile(path.resolve(__dirname, '../views/secret.html'))
+  } else {
+    res.send('You must be signed in to view this page')
+  }
 })
 
 app.post('/addTask', taskController.postTask)
 
 app.post('/deleteTask', taskController.deleteTask)
+
+app.post('/signin', authController.login)
 
 app.get('/getItems', taskController.getTasks)
 
