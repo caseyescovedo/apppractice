a12cae86257a57677916a6baa31dd6f7203b0b9e
bj-tseng <bonjay.tseng@gmail.com>
2020-08-22 15:27:00 -0400

    Completed functionality to automatically retrieve updated tasks list with task creation or deletion

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,33 +15,7 @@ getBtn.addEventListener('click', async (e) => {
           })
           .catch(err => console.log('Error with fetch request: ', err));
   
-  tasksList.forEach(taskObj => {
-    const newListItem = document.createElement('li');
-    newListItem.id = taskObj.id;
-    newListItem.innerHTML = taskObj.item;
-    
-    const newBtn = document.createElement('button');
-    newBtn.innerHTML = 'X';
-    newBtn.className = 'remove';
-    
-    newBtn.addEventListener('click', async (e) => {
-      e.preventDefault();
-      
-      newListItem.remove();
-
-      await fetch(`/delete?itemid=${newListItem.id}`, {
-                method: 'DELETE',
-              })
-              .then(res => res.json())
-              .then(res => console.log(res))
-              .catch(err => console.log('Error with fetch request: ', err));
-
-    })
-
-    newListItem.appendChild(newBtn);
-    const taskContainer = document.querySelector('#task-list');
-    taskContainer.appendChild(newListItem);
-  })
+  tasksList && createTaskList(tasksList);
 }, {once: true})
 
 
@@ -67,4 +41,61 @@ taskBtn.addEventListener('click', async (e) => {
             console.log('THIS IS THE CLIENT: ', res)
           })
           .catch(err => console.log('Error with fetch request: ', err));
+
+  let tasksList;
+
+  await fetch('/retrieve')
+          .then(res => res.json())
+          .then(res => {
+          tasksList = res;
+          })
+          .catch(err => console.log('Error with fetch request: ', err));
+  
+  tasksList && createTaskList(tasksList);
 })
+
+
+// Helper function to parse tasks into list items on the DOM
+
+function createTaskList(input) {
+  
+  const taskContainer = document.querySelector('#task-list');
+  taskContainer.innerHTML = '';
+  
+  input.forEach(taskObj => {
+    const newListItem = document.createElement('li');
+    newListItem.id = taskObj.id;
+    newListItem.innerHTML = taskObj.item;
+    
+    const newBtn = document.createElement('button');
+    newBtn.innerHTML = 'X';
+    newBtn.className = 'remove';
+    
+    newBtn.addEventListener('click', async (e) => {
+      e.preventDefault();
+      
+      newListItem.remove();
+
+      await fetch(`/delete?itemid=${newListItem.id}`, {
+                method: 'DELETE',
+              })
+              .then(res => res.json())
+              .then(res => console.log(res))
+              .catch(err => console.log('Error with fetch request: ', err));
+
+      let tasksList;
+
+      await fetch('/retrieve')
+              .then(res => res.json())
+              .then(res => {
+              tasksList = res;
+              })
+              .catch(err => console.log('Error with fetch request: ', err));
+      
+      tasksList && createTaskList(tasksList);
+    })
+
+    newListItem.appendChild(newBtn);
+    taskContainer.appendChild(newListItem);
+  })
+}
