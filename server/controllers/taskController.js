3e7507f195ec68046817743b2ba7c068e946bee0
Alexander Kim <akim3235@gmail.com>
2020-09-19 15:29:21 -0700

    Finished the app assessment.

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,30 @@
-module.exports = {
+const Item = require('../models/TaskModel')
 
+const controller = {};
 
-};
+controller.postTask = (req, res, next) => {
+  Item.create({ ...req.body }, (err, data) => {
+    if (err) return next(err);
+    res.locals.item = data;
+    return next();
+  });
+}
+
+controller.getTasks = (req, res, next) => {
+  Item.find({}, (err, data) => {
+    if (err) return next(err);
+    res.locals.items = data;
+    return next();
+  });
+}
+
+controller.deleteTask = (req, res, next) => {
+  const itemId = req.body.id;
+  Item.findByIdAndDelete(itemId, (err, data) => {
+    if (err) return next(err);
+    res.locals.item = data;
+    return next();
+  });
+}
+
+module.exports = controller;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI = 'mongodb+srv://akim:bankir1983@cluster0.lo2hn.mongodb.net/todolist?retryWrites=true&w=majority';
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
+const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 // const URI = process.env.PG_URI || myURI;
 
+// connect to MongoDB
+mongoose.connect(URI, {
+  // options for the connect method to deal with deprecation warnings
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+.then(()=>console.log('Connected to MongoDB.'))
+.catch(err=>console.log(err));
 
+// sets a schema for the 'Tasks' collection
+const itemSchema = new Schema({
+  item: { type: String, required: true },
+  created_at: { type: String, required: true },
+});
 
-
-module.exports = null; // <-- export your model
+module.exports = mongoose.model('Tasks', itemSchema);
