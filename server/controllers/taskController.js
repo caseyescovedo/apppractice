598a29b84b7be20f6aa88ff1ad0a15b67bf07a3d
VNguyenCode <vnguyencode@gmail.com>
2020-09-19 16:02:51 -0700

    need APC on AUTH

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,44 @@
-module.exports = {
+const db = require('../models/TaskModel');
 
+const controller = {};
 
+controller.postTask = (req, res, next) => {
+  let {variable} = req.body
+  let created_at = Date.now();
+  let query = 'INSERT INTO tasks (item,created_at) VALUES ($1, $2) returning *';
+
+  db.query(query, [variable, created_at], (err, post) => {
+    if (err) {
+      return next(err);
+    }
+    res.locals.post = post.rows[0];
+    return next();
+  });
 };
+
+controller.getTasks = (req, res, next) => {
+  let query = 'SELECT * FROM tasks';
+  db.query(query)
+    .then((data) => {
+      res.locals.get = data.rows;
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
+
+controller.deleteTask = (req, res, next) => {
+  let { id } = req.body;
+  let query = 'DELETE from tasks where _id = $1 RETURNING _id';
+  db.query(query, [id], (err, response) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.locals.deleted = response.rows[0];
+      return next();
+    }
+  });
+};
+
+module.exports = controller;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,20 @@
-// v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const {Pool} = require('pg')
 
+// v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
+const myURI = 'postgres://nfxquvlj:wepgIGcknbZ8cpCujXydfOYVmkReots4@lallah.db.elephantsql.com:5432/nfxquvlj';
+
+const pool = new Pool({connectionString:myURI});
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
 
 
 
 
-module.exports = null; // <-- export your model
+module.exports = {
+  query:function(text,params,cb){
+    return pool.query(text,params,cb)
+  }
+}; // <-- export your model
