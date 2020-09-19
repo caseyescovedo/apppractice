8587a1f524d5a45d484a39dde81ec2996b85e070
Lucy Chi <lucycchi@gmail.com>
2020-09-19 16:32:16 -0700

    not done

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,55 @@
-module.exports = {
+const { del } = require("request");
+const db = require("../models/TaskModel.js");
+const taskController = {};
 
+/* NOTES FROM TASKMODEL
+CREATE TABLE todo (_id SERIAL PRIMARY KEY, item VARCHAR(200), created_at TIMESTAMP);
+INSERT current time with CURRENT_TIMESTAMP*/
 
+//should create a new item in the database
+taskController.postTask = (req, res, next) => {
+  //tableItem should equal whatever arrives from client
+  const tableItem = req.body.item;
+  const params = [tableItem];
+  const insertQuery = `INSERT INTO todo (item, created_at) 
+    VALUES ($1, CURRENT_TIMESTAMP);`;
+  db.query(insertQuery, params)
+    .then((data) => {
+      console.log("posttask", data);
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    });
 };
+
+//should retrieve all items from the database and send it back to the client as JSON
+taskController.getTask = (req, res, next) => {
+  const queryAll = `SELECT * FROM todo;`;
+  db.query(queryAll)
+    .then((data) => {
+      res.locals.data = data;
+      console.log("gettask", data);
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
+
+//should find items in the database based on an ID number and delete that item if it exists
+taskController.deleteTask = (req, res, next) => {
+  const idNum = req.body.id;
+  const params = [idNum];
+  const deleteQuery = `DELETE FROM todo WHERE _id=$1;`;
+  db.query(deleteQuery, params)
+    .then((data) => {
+      console.log("deletetask", data);
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
+
+module.exports = taskController;
--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -1,13 +1,21 @@
 // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-const myURI = '';
+const myURI =
+  "postgres://kiggmsfb:trzqrhUWZ-8YQquyMU_2eswDSuV8S6jg@lallah.db.elephantsql.com:5432/kiggmsfb";
 
 // UNCOMMENT THE LINE BELOW IF USING MONGO
 // const URI = process.env.MONGO_URI || myURI;
 
 // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// const URI = process.env.PG_URI || myURI;
+const URI = process.env.PG_URI || myURI;
+const { Pool } = require("pg");
+const pool = new Pool({ connectionString: URI });
 
+module.exports = {
+  query: function (text, params, callback) {
+    return pool.query(text, params, callback);
+  },
+}; // <-- export your model
 
+//CREATE TABLE todo (_id SERIAL PRIMARY KEY, item VARCHAR(200), created_at TIMESTAMP);
 
-
-module.exports = null; // <-- export your model
+//insert current time with CURRENT_TIMESTAMP
