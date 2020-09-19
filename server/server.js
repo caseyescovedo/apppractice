63535bbfe4e0dfb21b2a51610373684a99b0807c
Casey Walker <casey.e.walker@gmail.com>
2020-09-19 15:00:25 -0700

    just have weird error to fix

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
-const { urlencoded } = require('body-parser');
 const express = require('express');
 const app = express();
 const PORT = 3333;
 const path = require('path');
 const taskController = require('./controllers/taskController');
-//parse req bodies
+const authController = require('./controllers/authController');
+const cookieParser = require('cookie-parser');
+
+//parse cookies and req bodies
+app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 
 //serve static files, content-type is set by express.static
 app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
 //serve secret.html on the /secret route, content-type set by sendFile
-app.get('/secret', (req, res) => {
+app.get('/secret', authController.checkCookie, (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../views/secret.html'));
 })
 
@@ -29,7 +32,7 @@ app.get('/api', taskController.getTasks, (req, res) => {
   res.status(200).json(res.locals.tasks);
 })
 
-app.use('/signin', (req, res) => {
+app.use('/signin', authController.authenticate, (req, res) => {
   //redirect to secret page upon signin
   res.status(200).redirect('/secret');
 })
